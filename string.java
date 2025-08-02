0aeab0003d3b7c54b962d95d62b48b8d8797def1
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-02 09:06:59 +0530

    Binary search using recursion

--- string.java
+++ string.java
@@ -1,9 +1,35 @@
+import java.util.Arrays;
+
 public class string {
     public static void main(String args[]){
+        String s = "hello";
        
-        String s = "null";
-        System.out.println(isPalindrom(s));
+      int left = 0;
+        int right = s.length -1;
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            right--;
+            left++;
+        }
+
+        System.out.println(s);
    
+    }
+
+    public static void reverse(String s){
+        int left = 0;
+        int right = s.length() -1;
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            right--;
+            left++;
+        }
     }
 
     public static boolean isPalindrom(String str){
